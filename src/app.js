1c39853e0c617559674db51a6794fd293e6a8bad
Drew <drew@drewbillings.net>
2020-08-05 16:48:17 -0700

    use non-blocking read and write to file

--- src/app.js
+++ src/app.js
@@ -5,32 +5,40 @@ const edit = document.getElementById("editor");
 const load = document.getElementById("btnLoad");
 const save = document.getElementById("btnSave");
 const blank = document.getElementById("btnNew");
-const pathField = document.getElementById("path");
 
 //create structural elements for blank a file
 const newDiv = document.createElement("DIV");
 const newBr = document.createElement("BR");
 
+//global vars
+let filePath = document.getElementById("path").innerText;
+
+//blank the editor
 blank.addEventListener("click", () => {
-	edit.innerText = ""; 
+	edit.innerText = "";
 	edit.appendChild(newDiv).appendChild(newBr);
 	stat.innerText = "Ready.";
 });
 
+//load the contents of a file to the editor
 load.addEventListener("click", () => {
-	var path = pathField.innerText;
-	data = fs.readFileSync( path, 'utf-8');
-	edit.innerText = data; 
+	fs.readFile(filePath, (err, data) => {
+		if (err) throw err;
+		edit.innerText = data;
+	});
 	stat.innerText = "Loaded.";
 });
 
+//save the editor contents to file
 save.addEventListener("click", () => {
- 	var path = pathField.innerText;
- 	var data = edit.innerText;
- 	fs.writeFileSync(path, data, "utf-8");
- 	stat.innerText = "Saved.";
+ 	let data = edit.innerText;
+ 	fs.writeFile(filePath, data, 'utf-8', (err) => {
+ 		if (err) throw err;
+ 		stat.innerText = "Saved.";
+ 	});
 });
 
+//listen for changes in the editor
 edit.addEventListener("keydown", () => {
 	stat.innerText = "Changed.";
 });
